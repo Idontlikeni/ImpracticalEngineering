8e3586667051e9e7cf06d4fede4d6b849c74e5ca
super <antvg05@yandex.ru>
2021-12-18 19:42:30 +0300

    added: ui, money

--- TowerDefence.py
+++ TowerDefence.py
@@ -1,18 +1,31 @@
 import pygame
 import math
 
-countx = 20
-county = 20
+countx = 34
+county = 23
 cellsize = 40
-width = countx * cellsize + 1
+width = countx * cellsize
 height = county * cellsize + 1
 fps = 60
 bullets = []
 towers = []
+towernum = 0
+uirect = [pygame.Rect(1361, 160, 240, 140), pygame.Rect(1361, 320, 240, 140),
+          pygame.Rect(1361, 480, 240, 140), pygame.Rect(1361, 640, 240, 140)]
 
 pygame.init()
-window = pygame.display.set_mode((width, height))
+pygame.font.init()
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+window = pygame.display.set_mode((width + 240, height))
 clock = pygame.time.Clock()
+
+
+class Player:
+    def __init__(self, money):
+        self.money = money
+
+    def money(self):
+        return self.money
 
 
 class Tower:
@@ -23,21 +36,51 @@ class Tower:
         self.color = color
         self.damage = damage
         self.firespeed = firespeed
-        self.fire_rate_tick = 0
+        self.firerate = 0
         self.price = price
         self.attackrad = attackrad
 
     def draw(self):
         pygame.draw.circle(window, self.color, [self.x, self.y], self.size)
 
+    def cost(self):
+        return self.price
+
 
 class CommonTower(Tower):
     def fire(self):
-        if self.fire_rate_tick <= 0:
+        if self.firerate <= 0:
             addbullet(self.x, self.y, 3, 'green', 5, 20)
-            self.fire_rate_tick = self.firespeed
+            self.firerate = self.firespeed
         else:
-            self.fire_rate_tick -= 1 / fps
+            self.firerate -= 1 / fps
+
+
+class CommonTower1(Tower):
+    def fire(self):
+        if self.firerate <= 0:
+            addbullet(self.x, self.y, 3, 'yellow', 5, 20)
+            self.firerate = self.firespeed
+        else:
+            self.firerate -= 1 / fps
+
+
+class CommonTower2(Tower):
+    def fire(self):
+        if self.firerate <= 0:
+            addbullet(self.x, self.y, 3, 'blue', 5, 20)
+            self.firerate = self.firespeed
+        else:
+            self.firerate -= 1 / fps
+
+
+class CommonTower3(Tower):
+    def fire(self):
+        if self.firerate <= 0:
+            addbullet(self.x, self.y, 3, 'red', 5, 20)
+            self.firerate = self.firespeed
+        else:
+            self.firerate -= 1 / fps
 
 
 class Bullet:
@@ -76,14 +119,57 @@ def windowrender():
     for i in range(countx + 1):
         pygame.draw.line(window, 'white', [i * cellsize, 0], [i * cellsize, height * cellsize])
     for i in range(county + 1):
-        pygame.draw.line(window, 'white', [0, i * cellsize], [width * cellsize, i * cellsize])
+        pygame.draw.line(window, 'white', [0, i * cellsize], [width, i * cellsize])
 
 
-def maketower():
+def ui():
+    pygame.draw.rect(window, 'black', [1361, 0, 240, 921])
+    pygame.draw.circle(window, 'green', [1482, 230], 60)
+    cost = myfont.render('10', False, 'white')
+    window.blit(cost, (1467, 285))
+    pygame.draw.circle(window, 'yellow', [1482, 410], 60)
+    cost = myfont.render('15', False, 'white')
+    window.blit(cost, (1467, 465))
+    pygame.draw.circle(window, 'red', [1482, 580], 60)
+    cost = myfont.render('30', False, 'white')
+    window.blit(cost, (1467, 635))
+    pygame.draw.circle(window, 'blue', [1482, 750], 60)
+    money = myfont.render(str(plr.money), False, 'white')
+    cost = myfont.render('50', False, 'white')
+    window.blit(cost, (1467, 805))
+    window.blit(money, (1461, 30))
+    if towernum == 0:
+        pygame.draw.rect(window, 'white', [1402, 160, 160, 170], 4)
+    if towernum == 1:
+        pygame.draw.rect(window, 'white', [1402, 340, 160, 170], 4)
+    if towernum == 2:
+        pygame.draw.rect(window, 'white', [1402, 510, 160, 170], 4)
+    if towernum == 3:
+        pygame.draw.rect(window, 'white', [1402, 680, 160, 170], 4)
+
+
+def uiswtch():
     x, y = getmpos()
-    if maycreatetower(x, y):
-        towers.append(CommonTower(x + cellsize / 2, y + cellsize / 2, 10, 'green', 5, 0.5, 10, 150))
-        print(towers)
+    for i in uirect:
+        if i.collidepoint((x, y)):
+            return uirect.index(i)
+
+
+def maketower(n):
+    x, y = getmpos()
+    if maycreatetower(x, y) and x <= width:
+        if n == 0 and plr.money >= 10:
+            towers.append(CommonTower(x + cellsize / 2, y + cellsize / 2, 10, 'green', 5, 0.5, 10, 150))
+            plr.money -= 10
+        if n == 1 and plr.money >= 15:
+            towers.append(CommonTower1(x + cellsize / 2, y + cellsize / 2, 10, 'yellow', 5, 0.5, 15, 150))
+            plr.money -= 15
+        if n == 3 and plr.money >= 50:
+            towers.append(CommonTower2(x + cellsize / 2, y + cellsize / 2, 10, 'blue', 5, 0.5, 30, 150))
+            plr.money -= 50
+        if n == 2 and plr.money >= 30:
+            towers.append(CommonTower3(x + cellsize / 2, y + cellsize / 2, 10, 'red', 5, 0.5, 50, 150))
+            plr.money -= 30
 
 
 def getmpos():
@@ -103,6 +189,8 @@ def run():
         bullet.draw()
 
 
+plr = Player(100)
+
 running = True
 while running:
     for event in pygame.event.get():
@@ -110,9 +198,12 @@ while running:
             run = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                maketower()
+                maketower(towernum)
+                if getmpos()[0] >= 1361:
+                    towernum = uiswtch()
     window.fill('black')
     run()
+    ui()
     pygame.display.update()
     clock.tick(fps)
 pygame.quit()
