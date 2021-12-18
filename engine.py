60c101cc100a383f75e240d7080195eb561e9323
Idontlikeni <tima.xxxyyy@mail.ru>
2021-12-18 17:57:18 +0300

    Updates
    Added Generation, collision, crosshair, etc

--- engine.py
+++ engine.py
@@ -1,4 +1,4 @@
-import pygame, math, os
+import pygame, math, os, random
 from pygame.locals import *
 
 global e_colorkey
@@ -15,30 +15,81 @@ def collision_test(object_1,object_list):
             collision_list.append(obj)
     return collision_list
 
+TILE_SIZE = 20
+
 class World:
-    def __init__(self):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
         self.objects = []
-        self.field = [[0] * 15 for _ in range(15)]
+        self.field = [['1'] * width for _ in range(height)]
+        self.tile_rects = []
 
     def generate_map(self):
-        for i in range(15):
-            self.field[i][0] = 1
-        for i in range(15):
-            self.field[i][14] = 1
-        for i in range(15):
-            self.field[0][i] = 1
-        for i in range(15):
-            self.field[14][i] = 1
+        for i in range(self.height):
+            self.field[i][0] = 'x'
+        for i in range(self.height):
+            self.field[i][self.width - 1] = 'x'
+        for i in range(self.width):
+            self.field[0][i] = 'x'
+        for i in range(self.width):
+            self.field[self.height - 1][i] = 'x'
+        
+        numer_of_cells = random.randint(self.width * self.height, self.width * self.height * 2)
 
-    def add_gameobject(self, Object):
+        x, y = random.randint(1, self.width - 2), random.randint(1, self.height - 2)
+        self.field[y][x] = '0'
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)] 
+
+        for i in range(numer_of_cells):
+            vec = directions[random.randint(0, 3)]
+            if self.field[y + vec[1]][x + vec[0]] == 'x':
+                for v in directions:
+                    if self.field[y + v[1]][x + v[0]] == '1' or self.field[y + v[1]][x + v[0]] == '2':
+                        self.field[y + v[1]][x + v[0]] = '0'
+                        y += v[1]
+                        x += v[0]
+            else:
+                self.field[y + vec[1]][x + vec[0]] = '0'
+                y += vec[1]
+                x += vec[0]
+            for v in directions:
+                if self.field[y + v[1]][x + v[0]] == '1':
+                    self.field[y + v[1]][x + v[0]] = '2'
+        
+        y = 0
+        for row in self.field:
+            x = 0
+            for tile in row:
+                if tile != '0' and tile != '1':
+                    self.tile_rects.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+                x += 1
+            y += 1
+        
+    def add_game_object(self, Object):
         self.objects.append(Object)
 
     def update(self):
         for object in self.objects:
             object.update()
     
-    def draw(self):
-        pass
+    def draw(self, display, scroll):
+        y = 0
+        for row in self.field:
+            x = 0
+            for tile in row:
+                if tile == '1':
+                    pygame.draw.rect(display, (255, 255, 255), pygame.Rect((x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]), (TILE_SIZE, TILE_SIZE)), 1)
+                    #  display.blit(dirt_image, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+                if tile == '2':
+                    pygame.draw.rect(display, (0, 255, 0), pygame.Rect((x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]), (TILE_SIZE, TILE_SIZE)), 1)
+                if tile == 'x':
+                    pygame.draw.rect(display, (255, 0, 0), pygame.Rect((x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]), (TILE_SIZE, TILE_SIZE)), 1)
+                x += 1
+            y += 1
+
+    def get_rects(self):
+        return self.tile_rects
 
 class GameObject:
     def __init__(self, x, y, width, height):
@@ -48,7 +99,7 @@ class GameObject:
         self.height = height
         self.rect = pygame.Rect(x, y, width, height)
     
-    def draw(self, is_debug=False):
+    def draw(self, display, scroll, is_debug=False):
         if is_debug:
             pygame.draw.rect(display, (255, 0, 0), self.rect, 1)
     
@@ -104,7 +155,85 @@ class Entity(GameObject):
         self.height = height
         self.type = type
         self.physical_object = PhysicalObject(x, y, width, height)
+        self.projectiles = []
+        self.shootTimer = 50
 
     def draw(self, surface, scroll):
         pygame.draw.rect(surface, (255, 0, 0), pygame.Rect(self.physical_object.x-scroll[0], self.physical_object.y-scroll[1],
                                                            self.physical_object.width, self.physical_object.height), 1)
+    
+    def set_pos(self,x,y):
+        self.x = x
+        self.y = y
+        self.physical_object.x = x
+        self.physical_object.y = y
+        self.physical_object.rect.x = x
+        self.physical_object.rect.y = y
+
+    def update(self):
+        if self.shootTimer < 50:
+            self.shootTimer += 1
+    
+    def move(self,movement,platforms):
+        collisions = self.physical_object.move(movement,platforms)
+        self.x = self.physical_object.x
+        self.y = self.physical_object.y
+        return collisions
+ 
+    def rect(self):
+        return pygame.Rect(self.x,self.y,self.width,self.height)
+
+    def shoot(self, angle):
+        if self.shootTimer >= 50:
+            projectile = Projectile(self.x + self.width / 2 - 4, self.y + self.height / 2 - 4, 8, 8, angle, 5, "player_projectile")
+            self.projectiles.append(projectile)
+            self.shootTimer = 0
+    
+    def draw_projectiles(self, surface, scroll):
+        for projectile in self.projectiles:
+            projectile.draw(surface, scroll)
+
+    def move_projectiles(self, platforms):
+        for count, projectile in enumerate(self.projectiles):
+            collisions = projectile.move(platforms)
+            print(collisions)
+            if len(collisions['data']) > 0:
+                self.projectiles.remove(projectile)
+
+
+class Player(Entity):
+    def __init__(self, x, y, width, height, type):
+        super().__init__(x, y, width, height, type)
+
+class Cursor(GameObject):
+    def __init__(self, x, y, path):
+        self.x = x
+        self.y = y
+        self.img = pygame.image.load(path)
+        self.width = self.img.get_width()
+        self.height = self.img.get_height()
+
+    def set_pos(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self, display):
+        display.blit(self.img, (self.x - self.width // 2, self.y - self.height // 2))
+
+class Projectile(Entity):
+    def __init__(self, x, y, width, height, angle, velocity, type):
+        super().__init__(x, y, width, height, type)
+        self.angle = angle
+        self.velocity = velocity
+
+    def move(self, platforms):
+        movement = [math.cos(self.angle) * self.velocity, math.sin(self.angle) * self.velocity]
+        collisions = self.physical_object.move(movement,platforms)
+        self.x = self.physical_object.x
+        self.y = self.physical_object.y
+        if len(collisions) > 0:
+            del self
+        return collisions
+
+    def set_move_angle(self, angle):
+        self.angle = angle
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pygame, sys, os, random
+import pygame, sys, os, random, math
+
+from pygame import mouse
 import engine as e
 from pygame.locals import *
 from pygame.mixer import set_num_channels
@@ -7,43 +9,75 @@ pygame.init()
 pygame.display.set_caption("Yandex game")
 pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.mixer,set_num_channels(64)
+random.seed()
 clock = pygame.time.Clock()
 
-WINDOW_SIZE = (1920, 1080)
+WINDOW_SIZE = (1600, 900)
 TILE_SIZE = 16
+SCALE_MULTIPLIER = 5
+
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+clicked = False
+
+pygame.mouse.set_visible(False)
 
 moving_right = False
 moving_left = False
 moving_up = False
 moving_down = False
 
-
 true_scroll = [0,0]
 scroll = [0, 0]
-display = pygame.Surface((480, 270))
-world = e.World()
 
+display = pygame.Surface((WINDOW_SIZE[0] / SCALE_MULTIPLIER, WINDOW_SIZE[1] / SCALE_MULTIPLIER))
+world = e.World(48, 48)
 player = e.Entity(0,0,16,16,'player')
-
+cursor = e.Cursor(0, 0, 'data_img/curs3.png')
+world.generate_map()
 
 while True:
     display.fill((0, 0, 0))
-
-    true_scroll[0] += (player.x - true_scroll[0] - 480 / 2 + 8) / 20
-    true_scroll[1] += (player.y -true_scroll[1] - 270 / 2 + 8) / 20
+    mouse_pos = pygame.mouse.get_pos()
+    cursor.set_pos(mouse_pos[0] / SCALE_MULTIPLIER, mouse_pos[1] / SCALE_MULTIPLIER)
+    true_scroll[0] += (player.x - true_scroll[0] - display.get_width() / 2 + 8) / 10
+    true_scroll[1] += (player.y - true_scroll[1] - display.get_height() / 2 + 8) / 10
     scroll = true_scroll.copy()
     scroll[0] = int(scroll[0])
     scroll[1] = int(scroll[1])
+    mouse_angle = math.atan2(mouse_pos[1] / SCALE_MULTIPLIER - player.y - player.height / 2 + scroll[1], mouse_pos[0] / SCALE_MULTIPLIER - player.x - player.width / 2 + scroll[0])
 
     #  scroll = [0, 0]
 
-    world.draw()
+    tile_rects = world.get_rects()
 
+    world.draw(display, scroll)
+
+    player_movement = [0, 0]
+
+    if moving_right:
+        player_movement[0] += 2
+    if moving_left:
+        player_movement[0] -= 2
+    if moving_up:
+        player_movement[1] -= 2
+    if moving_down:
+        player_movement[1] += 2
+    if clicked == True:
+        player.shoot(mouse_angle)
+    
+    collision_types = player.move(player_movement,tile_rects)
+
+    player.move_projectiles(tile_rects)
+    player.update()
     player.draw(display,scroll)
 
+    player.draw_projectiles(display, scroll)
+
     #  player.move(player_movement)
-    pygame.draw.line(display, (0, 255, 0), (player.x + 8 - scroll[0], player.y + 8 - scroll[1]), ((pygame.mouse.get_pos()[0] // 4), (pygame.mouse.get_pos()[1] // 4)))
+    pygame.draw.line(display, (0, 255, 0), (player.x + player.width / 2 - scroll[0], player.y + player.height / 2 - scroll[1]), ((pygame.mouse.get_pos()[0] // SCALE_MULTIPLIER), (pygame.mouse.get_pos()[1] // SCALE_MULTIPLIER)))
+    
+    cursor.draw(display)
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -66,8 +100,15 @@ while True:
                 moving_up = False
             if event.key == K_s:
                 moving_down = False
-        
-    
+        if event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:
+                #  print(math.atan2(mouse_pos[1] / SCALE_MULTIPLIER - player.y, mouse_pos[0] / SCALE_MULTIPLIER - player.x))
+                clicked = True
+        if event.type == MOUSEBUTTONUP:
+            if event.button == 1:
+                clicked = False
+
+                
     world.update()
     screen.blit(pygame.transform.scale(display, WINDOW_SIZE), (0, 0))
     pygame.display.update()
